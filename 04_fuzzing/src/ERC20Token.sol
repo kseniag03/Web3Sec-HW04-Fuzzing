8f749467c8ff266e7450413c9616504e66684fcc
kseniag03 <114473740+kseniag03@users.noreply.github.com>
2024-12-14 16:54:56 +0300

    added generated contracts

---
+++ 04_fuzzing/src/ERC20Token.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^ 0.8.22;
+
+import { ERC20 } from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import { ERC20Burnable } from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { ERC20Pausable } from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import { ERC20Permit } from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import { Ownable } from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract ERC20Token is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+    ERC20("ERC20Token", "MTK")
+    Ownable(initialOwner)
+    ERC20Permit("ERC20Token")
+    { }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+    internal
+    override(ERC20, ERC20Pausable)
+    {
+        super._update(from, to, value);
+    }
+}
